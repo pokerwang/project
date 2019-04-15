d1bbc9e39ea2dd2803d8832d0b1c217537edb00a
pokerwang <476888706@qq.com>
2019-04-15 20:52:35 +0800

    first commit

---
+++ LinkedStack.java
@@ -0,0 +1,67 @@
+package test;
+import java.sql.*;
+public class LinkedStack {
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/day?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            //conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            conn =DriverManager.getConnection(DB_URL,USER,PASS);
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT UserId,UserName,UserPwd FROM UserInfo";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int UserId  = rs.getInt("UserId");
+                String UserName = rs.getString("UserName");
+                String UserPwd = rs.getString("UserPwd");
+
+                // 输出数据
+                System.out.print("ID: " + UserId);
+                System.out.print(",名称: " + UserName);
+                System.out.print(", 密码: " + UserPwd);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("END!");
+    }
+}
